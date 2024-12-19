f0b369a4ec217b99d0203d5eb866f190ea87bc30
billalxcode <billal.xcode@gmail.com>
2024-12-19 11:07:11 +0700

    add staking info

---
+++ ui/shared/staking/info.tsx
@@ -0,0 +1,8 @@
+export default function StakingInfo() {
+    return (
+        <div className='flex justify-between'>
+            <p className='font-bold'>APY Rewards</p>
+            <p className='font-semibold'>5%</p>
+        </div>
+    );
+}
