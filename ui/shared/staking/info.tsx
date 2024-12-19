bd0782ba2a1291dcea481d584fa62d9b426b2bb3
billalxcode <billal.xcode@gmail.com>
2024-12-19 18:06:08 +0700

    update staking info

--- ui/shared/staking/info.tsx
+++ ui/shared/staking/info.tsx
@@ -1,8 +1,22 @@
 export default function StakingInfo() {
     return (
-        <div className='flex justify-between'>
-            <p className='font-bold'>APY Rewards</p>
-            <p className='font-semibold'>5%</p>
+        <div className='flex flex-col gap-2'>
+            <div className='flex justify-between'>
+                <p className='font-bold'>APY Rewards</p>
+                <p className='font-semibold'>5%</p>
+            </div>
+            <div className='flex justify-between'>
+                <p className='font-bold'>Total Staked</p>
+                <p className='font-semibold'>0</p>
+            </div>
+            <div className='flex justify-between'>
+                <p className='font-bold'>My Staked</p>
+                <p className='font-semibold'>0</p>
+            </div>
+            <div className='flex justify-between'>
+                <p className='font-bold'>Rewards Earned</p>
+                <p className='font-semibold'>0</p>
+            </div>
         </div>
     );
 }
