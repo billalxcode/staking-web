8eed9ec14ff79c8c98672004c3c4de0e1345eecc
billalxcode <billal.xcode@gmail.com>
2024-12-19 18:06:01 +0700

    update staking form

--- ui/shared/staking/form.tsx
+++ ui/shared/staking/form.tsx
@@ -1,13 +1,21 @@
+'use client';
+import useToken from '@/states/features/token/hooks';
 import Button from '@/ui/components/button/button';
+import { formatUnits } from 'viem';
 
 export default function StakingForm() {
+    const { balance, symbol, decimals } = useToken();
+
     return (
         <div className='flex flex-col gap-2'>
             <div className='flex justify-between'>
                 <label htmlFor='amount' className='font-bold'>
                     Enter amount you want stake
                 </label>
-                <p className='font-semibold'>Balance: 0</p>
+                <p className='font-semibold'>
+                    Balance: {formatUnits(BigInt(balance), decimals ?? 18) ?? '-'}{' '}
+                    {symbol ?? ''}
+                </p>
             </div>
             <div className='flex justify-between border w-full p-2'>
                 <input
