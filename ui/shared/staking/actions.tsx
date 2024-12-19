03f6bf7153c39de1280e40f0bf1f6baf2935c221
billalxcode <billal.xcode@gmail.com>
2024-12-19 11:06:54 +0700

    add staking actions button

---
+++ ui/shared/staking/actions.tsx
@@ -0,0 +1,9 @@
+import Button from '@/ui/components/button/button';
+
+export default function StakingActions() {
+    return (
+        <Button size='lg' disabled>
+            Approve
+        </Button>
+    );
+}
