16cd9c25f6b48ad0ff396af4e45e134f2e3aad6e
billalxcode <billal.xcode@gmail.com>
2024-12-19 11:07:03 +0700

    add staking form

---
+++ ui/shared/staking/form.tsx
@@ -0,0 +1,23 @@
+import Button from '@/ui/components/button/button';
+
+export default function StakingForm() {
+    return (
+        <div className='flex flex-col gap-2'>
+            <div className='flex justify-between'>
+                <label htmlFor='amount' className='font-bold'>
+                    Enter amount you want stake
+                </label>
+                <p className='font-semibold'>Balance: 0</p>
+            </div>
+            <div className='flex justify-between border w-full p-2'>
+                <input
+                    type='text'
+                    name='amount'
+                    id='amount'
+                    className='w-full focus:outline-none'
+                />
+                <Button>Max</Button>
+            </div>
+        </div>
+    );
+}
