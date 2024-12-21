b84688a1e6d497cc43b62121275408c847932b6f
billalxcode <billal.xcode@gmail.com>
2024-12-21 11:14:05 +0700

    update wallet network

--- ui/shared/wallet/network.tsx
+++ ui/shared/wallet/network.tsx
@@ -1,25 +1,23 @@
 'use client';
-import { useEffect } from 'react';
+import { useDisclosure } from '@/hooks/useDisclosure';
 import { HiChevronDown } from 'react-icons/hi2';
-import { useAccount, useSwitchChain } from 'wagmi';
+import { useAccount } from 'wagmi';
+import NetworkSwitchModal from './switch';
 
 export default function NetworkSwitch() {
     const { chain, isConnected } = useAccount();
-    const { switchChain } = useSwitchChain();
-
-    useEffect(() => {
-        if (chain?.id !== 31337) {
-            switchChain({
-                chainId: 31337,
-            });
-        }
-    }, [chain?.id, switchChain]);
+    const { isOpen, onClose, onOpen } = useDisclosure();
 
     return isConnected ? (
-        <div className='flex gap-3 items-center border p-2 rounded-full px-4 transition duration-300 cursor-pointer'>
-            {chain?.name}
-
-            <HiChevronDown />
-        </div>
+        <>
+            <div
+                onClick={onOpen}
+                className='flex gap-3 items-center border p-2 rounded-full px-4 transition duration-300 cursor-pointer'
+            >
+                {chain?.name}
+                <HiChevronDown />
+            </div>
+            <NetworkSwitchModal isOpen={isOpen} onClose={() => onClose()} />
+        </>
     ) : null;
 }
