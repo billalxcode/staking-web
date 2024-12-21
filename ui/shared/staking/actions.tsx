8e3bad0aac1ea4888f5e6c9128736c83fd48c047
billalxcode <billal.xcode@gmail.com>
2024-12-21 14:00:29 +0700

    update staking component

--- ui/shared/staking/actions.tsx
+++ ui/shared/staking/actions.tsx
@@ -1,25 +1,12 @@
 import useApproval from '@/hooks/actions/useApproval';
 import useStake from '@/hooks/actions/useStake';
-import useAlert from '@/states/features/alert/hooks';
 import useStaking from '@/states/features/staking/hooks';
 import Button from '@/ui/components/button/button';
-import { useEffect } from 'react';
 
 export default function StakingActions() {
-    const { setAlertMessage } = useAlert();
-    const { action, errorMessage } = useStaking();
+    const { action } = useStaking();
     const { handleApprove } = useApproval();
     const { handleStake } = useStake();
-
-    useEffect(() => {
-        if (errorMessage) {
-            setAlertMessage({
-                message: errorMessage,
-                variant: 'danger',
-                duration: 3000,
-            });
-        }
-    }, [errorMessage, setAlertMessage]);
 
     if (action == 'approve') {
         return (
