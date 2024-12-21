dc828883588a8a2cef11d11ed43846b0c9fe87e6
billalxcode <billal.xcode@gmail.com>
2024-12-22 00:25:54 +0700

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,16 @@ export default {
                     800: 'rgba(122, 34, 201, 0.8)',
                     900: 'rgba(122, 34, 201, 0.9)',
                 },
+                dark: {
+                    background: "#1b1b1b",
+                    card60: "#2a2a2a28",
+                    border: "rgba(255, 255, 255, 0.1)",
+                    text: "rgba(255, 255, 255, 0.8)",
+                    border20: "rgba(255, 255, 255, 0.2)",
+                    duration: {
+                        background: "rgba(255, 255, 255, .1)"
+                    }
+                },
                 background: 'var(--background)',
                 foreground: 'var(--foreground)',
             },
