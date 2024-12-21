1cfe90e0fe5f2eb043a09017a0da2529915cd525
billalxcode <billal.xcode@gmail.com>
2024-12-21 11:13:58 +0700

    update staking info

--- ui/shared/staking/info.tsx
+++ ui/shared/staking/info.tsx
@@ -1,25 +1,40 @@
-import useStakingInfo from "@/hooks/useStakingInfo";
+import useStakingInfo from '@/hooks/useStakingInfo';
+import useToken from '@/states/features/token/hooks';
+import { formatUnits } from 'viem';
 
 export default function StakingInfo() {
-    const { } = useStakingInfo()
-    
+    const { decimals, symbol } = useToken();
+    const { apy, totalStakingTokens, myStaked, pendingReward } = useStakingInfo();
+
     return (
         <div className='flex flex-col gap-2'>
             <div className='flex justify-between'>
                 <p className='font-bold'>APY Rewards</p>
-                <p className='font-semibold'>5%</p>
+                <p className='font-semibold'>{parseInt(apy) / 100}%</p>
             </div>
             <div className='flex justify-between'>
                 <p className='font-bold'>Total Staked</p>
-                <p className='font-semibold'>0</p>
+                <p className='font-semibold'>
+                    {formatUnits(BigInt(totalStakingTokens), decimals ?? 18) ??
+                        '-'}{' '}
+                    {symbol ?? ''}
+                </p>
             </div>
             <div className='flex justify-between'>
                 <p className='font-bold'>My Staked</p>
-                <p className='font-semibold'>0</p>
+                <p className='font-semibold'>
+                    {formatUnits(BigInt(myStaked), decimals ?? 18) ??
+                        '-'}{' '}
+                    {symbol ?? ''}
+                </p>
             </div>
             <div className='flex justify-between'>
-                <p className='font-bold'>Rewards Earned</p>
-                <p className='font-semibold'>0</p>
+                <p className='font-bold'>Pending Reward</p>
+                <p className='font-semibold'>
+                {formatUnits(BigInt(pendingReward), decimals ?? 18) ??
+                        '-'}{' '}
+                    {symbol ?? ''}
+                </p>
             </div>
         </div>
     );
