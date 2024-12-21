8bd2a08f3eeb55c216f7304f926c3c3b630c676b
billalxcode <billal.xcode@gmail.com>
2024-12-21 21:18:08 +0700

    add hooks withdraw action

---
+++ hooks/actions/useWithdraw.ts
@@ -0,0 +1,63 @@
+import { useCallback } from "react";
+import useStakingContract from "../useStakingContract";
+import { usePublicClient, useWriteContract } from "wagmi";
+import useAlert from "@/states/features/alert/hooks";
+import { Abi } from "viem";
+
+export default function useWithdraw() {
+    const { staking_contract } = useStakingContract()
+    const { setAlertMessage } = useAlert()
+    const { writeContractAsync } = useWriteContract()
+    const publicClient = usePublicClient()
+
+    const handleWithdraw = useCallback(async (method: string) => {
+        setAlertMessage({
+            message: "Please confirm your wallet",
+            variant: 'loading'
+        }, false)
+        const tx = await writeContractAsync({
+            address: staking_contract?.address as `0x${string}`,
+            abi: staking_contract?.abi as Abi,
+            functionName: method
+        })
+        setAlertMessage(
+            {
+                message: 'Wait until the transaction is confirmed',
+                variant: 'loading',
+            },
+            false,
+        );
+        await publicClient?.waitForTransactionReceipt({ hash: tx });
+        setAlertMessage(
+            {
+                message: 'Transaction has been confirmed',
+                variant: 'success',
+                duration: 3000,
+            },
+            true,
+        );
+    }, [
+        setAlertMessage,
+        writeContractAsync,
+        staking_contract?.abi,
+        staking_contract?.address,
+        publicClient
+    ])
+
+    const handleNormalWithdraw = useCallback(async () => {
+        await handleWithdraw("withdraw")
+    }, [
+        handleWithdraw
+    ])
+    
+    const handleEmergencyWithdraw = useCallback(async () => {
+        await handleWithdraw("emergencyWithdraw")
+    }, [
+        handleWithdraw
+    ])
+    
+    return {
+        handleNormalWithdraw,
+        handleEmergencyWithdraw
+    }
+}
