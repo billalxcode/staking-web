aafe102faea3e6b43c5ae74d2b005a17946f034e
billalxcode <billal.xcode@gmail.com>
2024-12-22 23:50:09 +0700

    add claim reward

---
+++ hooks/actions/useClaimReward.ts
@@ -0,0 +1,54 @@
+import useAlert from '@/states/features/alert/hooks';
+import { useCallback } from 'react';
+import { Abi } from 'viem';
+import { usePublicClient, useWriteContract } from 'wagmi';
+import useStakingContract from '../useStakingContract';
+
+export default function useClaimReward() {
+    const { staking_contract } = useStakingContract();
+    const { setAlertMessage } = useAlert();
+    const { writeContractAsync } = useWriteContract();
+    const publicClient = usePublicClient();
+
+    const handleClaimReward = useCallback(async () => {
+        setAlertMessage(
+            {
+                message: 'Please confirm your wallet',
+                variant: 'loading',
+            },
+            false,
+        );
+        const tx = await writeContractAsync({
+            address: staking_contract?.address as `0x${string}`,
+            abi: staking_contract?.abi as Abi,
+            functionName: 'deposit',
+            args: ['0'],
+        });
+        setAlertMessage(
+            {
+                message: 'Wait until the transaction is confirmed',
+                variant: 'loading',
+            },
+            false,
+        );
+        await publicClient?.waitForTransactionReceipt({ hash: tx });
+        setAlertMessage(
+            {
+                message: 'Transaction has been confirmed',
+                variant: 'success',
+                duration: 3000,
+            },
+            true,
+        );
+    }, [
+        setAlertMessage,
+        writeContractAsync,
+        staking_contract?.abi,
+        staking_contract?.address,
+        publicClient,
+    ]);
+
+    return {
+        handleClaimReward,
+    };
+}
