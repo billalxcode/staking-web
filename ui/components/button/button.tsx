c889ffade809024b4b2d2665aa40ac78cf1f58b7
billalxcode <billal.xcode@gmail.com>
2024-12-22 23:50:23 +0700

    fix button

--- ui/components/button/button.tsx
+++ ui/components/button/button.tsx
@@ -14,7 +14,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
             primary:
                 'bg-primary-900 text-white hover:bg-primary disabled:bg-primary-600 dark:disabled:bg-primary-300',
             secondary:
-                'bg-gray-500 text-white hover:bg-gray-600 disabled:bg-gray-400 dark:disabled:bg-gray-200',
+                'bg-gray-500 text-white hover:bg-gray-600 disabled:bg-gray-400 dark:disabled:bg-dark-duration-background',
             danger: 'bg-red-500 text-white hover:bg-red-600 disabled:bg-red-600',
         };
 
