a455cd5a3df82bd810ef75cc2778fdf4d89acb76
billalxcode <billal.xcode@gmail.com>
2024-12-23 09:13:24 +0700

    add fixed number

--- ui/shared/staking/info.tsx
+++ ui/shared/staking/info.tsx
@@ -4,19 +4,19 @@ import { formatUnits } from 'viem';
 
 export default function StakingInfo() {
     const { decimals, symbol } = useToken();
-    const { apy, totalStakingTokens, myStaked, pendingReward } =
+    const { apy, totalStakingTokens, myStaked, pendingReward, rewardRemaining } =
         useStakingInfo();
 
     return (
         <div className='flex flex-col gap-2'>
             <div className='flex justify-between'>
                 <p className='font-bold'>APY Rewards</p>
-                <p className='font-semibold'>{parseInt(apy)}%</p>
+                <p className='font-semibold'>{isNaN(parseInt(apy)) ? "0" : parseInt(apy)}%</p>
             </div>
             <div className='flex justify-between'>
                 <p className='font-bold'>Total Staked</p>
                 <p className='font-semibold'>
-                    {formatUnits(BigInt(totalStakingTokens), decimals ?? 18) ??
+                    {Number(formatUnits(BigInt(totalStakingTokens), decimals ?? 18)).toFixed(4) ??
                         '-'}{' '}
                     {symbol ?? ''}
                 </p>
@@ -24,17 +24,25 @@ export default function StakingInfo() {
             <div className='flex justify-between'>
                 <p className='font-bold'>My Staked</p>
                 <p className='font-semibold'>
-                    {formatUnits(BigInt(myStaked), decimals ?? 18) ?? '-'}{' '}
+                    {Number(formatUnits(BigInt(myStaked), decimals ?? 18)).toFixed(4) ?? '-'}{' '}
                     {symbol ?? ''}
                 </p>
             </div>
             <div className='flex justify-between'>
                 <p className='font-bold'>Pending Reward</p>
                 <p className='font-semibold'>
-                    {formatUnits(BigInt(pendingReward), decimals ?? 18) ?? '-'}{' '}
+                    {Number(formatUnits(BigInt(pendingReward), decimals ?? 18)).toFixed(4) ?? '-'}{' '}
                     {symbol ?? ''}
                 </p>
             </div>
+            <div className='flex justify-between'>
+                <p className='font-bold'>Rewards Remaining</p>
+                <p className='font-semibold'>
+                    {Number(formatUnits(BigInt(rewardRemaining), decimals ?? 18)).toFixed(4) ?? '-'}{' '}
+                    {symbol ?? ''}
+                </p>
+            </div>
+            
         </div>
     );
 }
